a39dcaaaa2b07b6a147ccebfa5b1c61fc9a91877
Rob Jones <robert.buckingham.jones@gmail.com>
2013-01-05 09:40:38 +0000

    update

--- javascripts/audiobrain.js
+++ javascripts/audiobrain.js
@@ -1,5 +1,5 @@
 (function() {
-  var animate, audio, generateSentence, generateWords, randNum, randOperator, setDelay, speak, th, _i, _sentence, _words;
+  var animate, audio, enablePlay, generateSentence, generateWords, randNum, randOperator, setDelay, speak, th, togglePlay, _i, _playing, _sentence, _words;
 
   _i = 0;
 
@@ -7,8 +7,13 @@
 
   _sentence = [];
 
+  _playing = false;
+
   animate = function() {
     var delay, phrase, word;
+    if (_playing === false) {
+      return;
+    }
     if (_.isEmpty(_sentence)) {
       _sentence = generateSentence();
     }
@@ -87,13 +92,31 @@
     return audio(word).play();
   };
 
+  enablePlay = function() {
+    $('#playpause').removeAttr('disabled');
+    return $('#playpause').click(function() {
+      return togglePlay();
+    });
+  };
+
+  togglePlay = function() {
+    if (_playing) {
+      _playing = false;
+      return $('#playpause').text("Play");
+    } else {
+      _playing = true;
+      $('#playpause').text("Pause");
+      return animate();
+    }
+  };
+
   th = ['', 'thousand', 'million', 'billion', 'trillion'];
 
   var dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine']; var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen']; var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety']; function toWords(s){s = s.toString(); s = s.replace(/[\, ]/g,''); if (s != parseFloat(s)) return 'not a number'; var x = s.indexOf('.'); if (x == -1) x = s.length; if (x > 15) return 'too big'; var n = s.split(''); var str = ''; var sk = 0; for (var i=0; i < x; i++) {if ((x-i)%3==2) {if (n[i] == '1') {str += tn[Number(n[i+1])] + ' '; i++; sk=1;} else if (n[i]!=0) {str += tw[n[i]-2] + ' ';sk=1;}} else if (n[i]!=0) {str += dg[n[i]] +' '; if ((x-i)%3==0) str += 'hundred ';sk=1;} if ((x-i)%3==1) {if (sk) str += th[(x-i-1)/3] + ' ';sk=0;}} if (x != s.length) {var y = s.length; str += 'point '; for (var i=x+1; i<y; i++) str += dg[n[i]] +' ';} return str.replace(/\s+/g,' ');};
 
 
   $(document).ready(function() {
-    return animate();
+    return enablePlay();
   });
 
 }).call(this);
